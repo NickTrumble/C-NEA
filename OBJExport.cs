dc6cfcb1b3f008333b22b419ac0a31ad9e8bfe44
NickTrumble <146560405+NickTrumble@users.noreply.github.com>
2024-09-28 20:42:05 +0100

    Update OBJExport.cs

--- OBJExport.cs
+++ OBJExport.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -60,24 +60,24 @@ namespace CSTerrain
             return faces;
         }
 
-        //generates a layer of water at a certain height in the obj file
-        public List<(float, float, float)> Gen_water(float scale)
-        {
-            float waterlevel = 0.4f;
-            List<(float, float, float)> vertices = new List<(float, float, float)>();
+        ////generates a layer of water at a certain height in the obj file
+        //public List<(float, float, float)> Gen_water(float scale)
+        //{
+        //    float waterlevel = 0.4f;
+        //    List<(float, float, float)> vertices = new List<(float, float, float)>();
 
-            for (int i = 0; i < row; i++)
-            {
-                for (int j = 0; j < col; j++)
-                {
-                    float x = i * scale;
-                    float y = waterlevel;
-                    float z = j * scale;
-                    vertices.Add((x, y, z));
-                }
-            }
-            return vertices;
-        }
+        //    for (int i = 0; i < row; i++)
+        //    {
+        //        for (int j = 0; j < col; j++)
+        //        {
+        //            float x = i * scale;
+        //            float y = waterlevel;
+        //            float z = j * scale;
+        //            vertices.Add((x, y, z));
+        //        }
+        //    }
+        //    return vertices;
+        //}
 
         //combines all functions to one to export the map
         public void Export(string path, float scale)
@@ -85,23 +85,17 @@ namespace CSTerrain
             int multiplyer = 2;
             StreamWriter f = new StreamWriter(path + "\\terrain.obj");
             List<(float, float, float)> vertices = Generate_vertices(scale);
-            List<(float, float, float)> watervertices = Gen_water(scale);
             List<(int, int, int)> faces = Gen_faces();
-            MessageBox.Show($"faces:{faces.Count()} vertices:{vertices.Count()} water:{watervertices.Count()}");
+            MessageBox.Show($"faces:{faces.Count()} vertices:{vertices.Count()}");
 
             foreach (var v in vertices)
             {
                 f.WriteLine($"v {v.Item1} {v.Item2 * multiplyer} {v.Item3}");
             }
-            foreach (var v in watervertices)
-            {
-                f.WriteLine($"v {v.Item1} {v.Item2 * multiplyer} {v.Item3}");
-            }
-            int wateroffset = vertices.Count();
+
 
             foreach (var face in faces)
             {
-                f.WriteLine($"f {face.Item1 + wateroffset} {face.Item2 + wateroffset} {face.Item3 + wateroffset}");
                 f.WriteLine($"f {face.Item1} {face.Item2} {face.Item3}");
             }
             _ = MessageBox.Show("saved");
