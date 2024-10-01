c3b5e32ffcb92cf1e9e0f1f9e77def7fec38a031
NickTrumble <146560405+NickTrumble@users.noreply.github.com>
2024-10-01 15:51:29 +0100

    Update OBJExport.cs

--- OBJExport.cs
+++ OBJExport.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Collections.Concurrent;
 
 namespace CSTerrain
 {
@@ -25,8 +26,8 @@ namespace CSTerrain
         //generates the vertices used in the obj file 
         public List<(float, float, float)> Generate_vertices(float scale)
         {
-            List<(float, float, float)> vertices = new List<(float, float, float)>();
-            for (int i = 0; i < row; i++)
+            ConcurrentBag<(float, float, float)> vertices = new ConcurrentBag<(float, float, float)>();
+            Parallel.For(0, row, i =>
             {
                 for (int j = 0; j < col; j++)
                 {
@@ -35,16 +36,16 @@ namespace CSTerrain
                     float z = j * scale;
                     vertices.Add((x, y, z));
                 }
-            }
-            return vertices;
+            });
+            return vertices.ToList();
         }
 
         //generates a list of indexed vertices connected to make faces
         public List<(int, int, int)> Gen_faces()
         {
-            List<(int, int, int)> faces = new List<(int, int, int)>();
+            ConcurrentBag<(int, int, int)> faces = new ConcurrentBag<(int, int, int)>();
 
-            for (int i = 0; i < row - 2; i++)
+            Parallel.For(0, row - 2, i =>
             {
                 for (int j = 0; j < col - 2; j++)
                 {
@@ -55,48 +56,28 @@ namespace CSTerrain
                     faces.Add((tl, tr, br));
                     faces.Add((tl, br, bl));
                 }
-            }
+            });
 
-            return faces;
+            return faces.ToList();
         }
 
-        ////generates a layer of water at a certain height in the obj file
-        //public List<(float, float, float)> Gen_water(float scale)
-        //{
-        //    float waterlevel = 0.4f;
-        //    List<(float, float, float)> vertices = new List<(float, float, float)>();
-
-        //    for (int i = 0; i < row; i++)
-        //    {
-        //        for (int j = 0; j < col; j++)
-        //        {
-        //            float x = i * scale;
-        //            float y = waterlevel;
-        //            float z = j * scale;
-        //            vertices.Add((x, y, z));
-        //        }
-        //    }
-        //    return vertices;
-        //}
-
         //combines all functions to one to export the map
-        public void Export(string path, float scale)
+        public async Task Export(string path, float scale)
         {
             int multiplyer = 2;
             StreamWriter f = new StreamWriter(path + "\\terrain.obj");
             List<(float, float, float)> vertices = Generate_vertices(scale);
             List<(int, int, int)> faces = Gen_faces();
-            MessageBox.Show($"faces:{faces.Count()} vertices:{vertices.Count()}");
+            //MessageBox.Show($"faces:{faces.Count()} vertices:{vertices.Count()}");
 
             foreach (var v in vertices)
             {
-                f.WriteLine($"v {v.Item1} {v.Item2 * multiplyer} {v.Item3}");
+                await f.WriteLineAsync($"v {v.Item1} {v.Item2 * multiplyer} {v.Item3}");
             }
-
-
+            
             foreach (var face in faces)
             {
-                f.WriteLine($"f {face.Item1} {face.Item2} {face.Item3}");
+                await f.WriteLineAsync($"f {face.Item1} {face.Item2} {face.Item3}");
             }
             _ = MessageBox.Show("saved");
 
