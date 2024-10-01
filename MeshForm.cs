5db3b8d340fc6b596f790141445cf7b6e799e09a
NickTrumble <146560405+NickTrumble@users.noreply.github.com>
2024-10-01 11:55:30 +0100

    Update MeshForm.cs

--- MeshForm.cs
+++ MeshForm.cs
@@ -18,9 +18,11 @@ namespace CSTerrain
         int scale = 8;
         int xoffset, yoffset;
         Label heightlabel;
+        Bitmap b;
         public MeshForm()
         {
             InitializeComponent();
+            this.DoubleBuffered = true;
             this.Width = 1000;
             this.Height = 800;
             xoffset = Width / 2;
@@ -45,26 +47,6 @@ namespace CSTerrain
             Controls.Add(heightlabel);
         }
 
-        private void HeightMouseMove(object sender, MouseEventArgs e)
-        {
-            // X = xoffset + (x - y) * tilewidth / 2;
-            // Y = yoffset + (x + y) * tileheight / 2 - z * 8;
-            // x - y = 2 *(X - xoffset) / tilewidth
-            // x + y - heightmap[x,y] * 8 = 2 * (Y - yoffset) / tileheight
-        //    int tilewidth = 1000 / heightmap.GetLength(0);
-        //    int tileheight = 800 / heightmap.GetLength(1);
-
-        //    int mx = e.Location.X;
-        //    int my = e.Location.Y;
-
-        //    int X = xoffset + (mx - my) * tilewidth / 2;
-        //    int Y = yoffset + (mx + my) * tileheight / 2 - heightmap[mx, my] * scale;
-        //    if (X > heightmap.GetLength(0) - 1 || X < 0 || Y < 0 || Y > heightmap.GetLength(1) - 1)
-        //    {
-        //        return;
-        //    }
-        //    heightlabel.Text = $"[{X}, {Y}] = {((heightmap[X, Y].ToString().Length > 4) ? heightmap[X, Y].ToString().Substring(0, 5) : heightmap[X, Y].ToString())}";
-        }
 
         public int[,] FloatToInt(float[,] Input)
         {
@@ -84,12 +66,23 @@ namespace CSTerrain
         {
             heightmap = FloatToInt(h);
             drawer = new DrawMesh(heightmap, xoffset, yoffset, scale);
+            CreateBitmap();
+            Invalidate();
+        }
+
+        public void CreateBitmap()
+        {
+            b = new Bitmap(Width, Height);
+            Graphics g = Graphics.FromImage(b);
+            drawer.Draw(g);
         }
 
         private void PaintHandler(object sender, PaintEventArgs e)
         {
-            drawer.Draw(e.Graphics);
-            this.MouseMove += new MouseEventHandler(HeightMouseMove);
+            if (b != null)
+            {
+                e.Graphics.DrawImage(b, 0, 0);
+            }
         }
 
         private void HomeForm(object sender, EventArgs e)
