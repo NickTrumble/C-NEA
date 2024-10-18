71ce31222532c0b18e628fbbaa02ed5f781c3577
NickTrumble <146560405+NickTrumble@users.noreply.github.com>
2024-10-18 12:36:19 +0100

    Update TerrainCmap.cs

--- CSTerrain/TerrainCmap.cs
+++ CSTerrain/TerrainCmap.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -19,19 +19,86 @@ namespace CSTerrain
             Color.White
         };
 
-        //public static Color GetBiomeC()
-        //{
-        //    Color[] colours =
-        //    {
-        //        Color.FromArgb(0, 255, 0), Color.FromArgb(0, 200, 150),
-        //        Color.FromArgb(200, 200, 0)
-        //    };
-        //}
+        public static Color GetBiomeC(int i, int j)
+        {
+            Color[] colours =
+            {
+                //forest, rainforest, desert, tundra, savanah, arcitc, taiga, grass, mediterainian
+                Color.FromArgb(34, 139, 34), Color.FromArgb(0, 198, 34),
+                Color.FromArgb(255, 202, 113), Color.FromArgb(114,140,116),
+                Color.FromArgb(219, 113, 7), Color.FromArgb(209, 230, 230),
+                Color.FromArgb(0, 100, 0), Color.FromArgb(124, 252, 0),
+                Color.FromArgb(254, 205, 50)
+            };
+            //temp  moist
+            //0.75  0.75    rainforest
+            //0.7   0.2     desert
+            //0.6   0.3 0.4 medit   savanah
+            //0.5   0.4 0.5 grass   temp forest
+            //0.4   0.3     taiga
+            //0.2   0.3     tundra
+            //0.1   0.2     arctic
+
+            
+            float moist = Manager.moisturemap[i, j];
+            float hieght = Manager.noisemap[i, j];
+            float temp = Manager.tempmap[i, j];
+            float abs = moist * moist + temp * temp;
+            if (moist >= 0.75 && temp >= 0.75)
+            {
+                // rainforest
+                float t = (abs - 0.75f) / 0.25f;
+            }
+            else if (temp >= 0.7 && moist <= 0.2)
+            {
+                // deset
+                float t = (hieght - 0.7f) / 0.05f;
+            }
+            else if (temp >= 0.6 && moist <= 0.3)
+            {
+                if (moist <= 0.3)
+                {
+                    // medit
+                    float t = (hieght - 0.6f) / 0.1f;
+                } else if (moist <= 0.4)
+                {
+                    //savanah
+                    float t = (hieght - 0.6f) / 0.1f;
+                }
+            }
+            else if (temp >= 0.5 && moist <= 0.4)
+            {
+                if (moist <= 0.4)
+                {
+                    // grass
+                    float t = (hieght - 0.1f) / 0.2f;
+                } else if (moist <= 0.5)
+                {
+                    // temp forest
+                    float t = (hieght - 0.1f) / 0.2f;
+                }
+            }
+            else if (temp <= 0.1 && moist <= 0.2)
+            {
+                // arctic
+                float t = (abs) / 0.1f;
+            }
+            else if (temp <= 0.2 && moist <= 0.3)
+            {
+                // tundra
+                float t = (abs - 0.1f) / 0.1f;
+            }
+            else if (temp <= 0.4 && moist >= 0.3)
+            {
+                // taiga
+                float t = (abs - 0.2f) / 0.2f;
+            }
+        }
 
         //interpolates between base colours in the cmap depending on the noise value
-        public static Color Interpolate_value(float noise_value)
+        public static Color Interpolate_value(int i, int j)
         {
-
+            float noise_value = Manager.noisemap[i, j];
             if (noise_value < 0.1) // waterd
             {
                 return cmapC[0];
